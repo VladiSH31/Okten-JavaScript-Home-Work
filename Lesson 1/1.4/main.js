edabc69eeba9be55097372162384159e55d52ae1
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-04 21:03:13 +0200

    -task 1.4 done -task 1.3 done -task 1.2 done -task 1.1 done

---
+++ Lesson 1/1.4/main.js
@@ -0,0 +1,9 @@
+let firstName = prompt('Як тебе звати?');
+
+let middleName = prompt('Як по батькові?');
+
+let age = prompt("Скількі тобі років?");
+
+let person = `${firstName} ${middleName} ${age}`;
+
+console.log(person);
