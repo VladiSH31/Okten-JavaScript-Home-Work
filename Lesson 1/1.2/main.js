7eb37d727e273d450964f40f54f8d5dbf4cd3690
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-04 20:38:16 +0200

    -task 1.2 done -task 1.1 done

---
+++ Lesson 1/1.2/main.js
@@ -0,0 +1,15 @@
+let firstName = 'Vladyslav';
+
+let middleName = 'Mykolayovych';
+
+let lastName = 'Sheremet';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+
+console.log(person);
+
+// або другий варіант
+
+let person2 = firstName + " " + middleName + " " + lastName;
+
+console.log(person2);
