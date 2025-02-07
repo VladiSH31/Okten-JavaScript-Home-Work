b934116683e8a65fcf904752428633fd75dcdbcb
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-07 08:57:36 +0200

    -add Lesson 2 -made 4 task

---
+++ Lesson 2/main.js
@@ -0,0 +1,208 @@
+// -------- #67kfznmiMl ---------- //
+
+let arr = [1, 11, 111, 111, 'one', 'eleven', true, false, 2, 99999];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+// --------  #LARqoUj5I and #sA3Gg1sCp ---------- //
+
+let book1 = {
+    title: 'Kobzar',
+    pageCount: 1000,
+    genre: 'Poetry',
+    authors: [
+        {
+            name:'Taras Shevchenko',
+            age: 70,
+        },
+        {
+            name:'Shevchenko Taras',
+            age: 75,
+        }
+    ],
+}
+
+let book2 = {
+    title: 'Eneida',
+    pageCount: 900,
+    genre: 'Poem',
+    authors: [
+        {
+            name: 'Ivan Kotlyarevsky',
+            age: 75,
+        }
+    ]
+}
+
+let book3 = {
+    title: 'Lisova Pisnya',
+    pageCount: 800,
+    genre: 'Poetry',
+    authors: [
+        {
+            name: 'Lesya Ukraine',
+            age: 50,
+        }
+    ]
+}
+
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
+// ------------------ #jCHFnEbdmFd -----------------------
+
+let newArray = [
+
+    user1 = {
+        login: "user1",
+        password: '1234qwe',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user2 = {
+        login: "user1",
+        password: 'sbfngfsn',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user3 = {
+        login: "user1",
+        password: 'xnvnxhmh',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user4 = {
+        login: "user1",
+        password: 'xufmumf',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user5 = {
+        login: "user1",
+        password: 'aewfwvv',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user6 = {
+        login: "user1",
+        password: '1234acsdava',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user7 = {
+        login: "user1",
+        password: '5364634qwe',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user8 = {
+        login: "user1",
+        password: '1234hgdhtwe',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user9 = {
+        login: "user1",
+        password: '123435ggetwe',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    },
+
+    user10 = {
+        login: "user1",
+        password: 'afbfabre32r4',
+        firstName: 'Ivan',
+        lastName: 'Ivanenko',
+        age: 31,
+        email: 'example@gmail.com',
+        gender: 'male',
+        covidStatus: true,
+        skills: ['html', 'css', 'js'],
+        nationality: 'ukrainian',
+    }
+
+]
+
+console.log(user1.password);
+console.log(user2.password);
+console.log(user3.password);
+console.log(user4.password);
+console.log(user5.password);
+console.log(user6.password);
+console.log(user7.password);
+console.log(user8.password);
+console.log(user9.password);
+
+// #coYydZuaeEB
