b6ec270691c381471e35183d0f87c839d8b13111
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-11 03:55:45 +0200

    -made task-2

--- Lesson 3/main.js
+++ Lesson 3/main.js
@@ -1,6 +1,17 @@
 // #y7crMeFwHcS
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// noinspection LanguageDetectionInspection
 
 for (let i = 0; i<10; i++) {
     document.write(`<div>Lorem ipsum dolor.</div>`)
 }
+// ------
+
+// #TYj7ncx
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+
+for (let i = 0; i<10; i++) {
+    document.write(`<div>Lorem ipsum dolor.${i}</div>`)
+}
+//Спочатку індекс поставив поряд з дівом, Передивився розв'язок, перемістив в середину. Не правильно зрозумів завдання.
