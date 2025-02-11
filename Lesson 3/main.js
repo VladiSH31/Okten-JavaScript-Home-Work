d7835120feab0a853a8e469489577db97f4ec5a3
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-11 12:48:47 +0200

    -made task - 7

--- Lesson 3/main.js
+++ Lesson 3/main.js
@@ -141,3 +141,73 @@ for (const product of products) {
     );
 
 }
+
+// #4WrHwFTEop0
+// є масив
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+//     - користувачів зі статусом true
+
+for (const user of users) {
+
+ if (user.status === true) {
+            console.log(user);
+        }
+
+}
+
+console.log(' ');
+
+// - користувачів зі статусом false
+
+for (const user of users) {
+
+    if (user.status === false) {
+        console.log(user);
+    }
+
+}
+
+console.log(' ');
+
+// - користувачів які старші за 30 років
+
+for (const user of users) {
+
+    if (user.age > 30) {
+        console.log(user);
+    }
+
+}
+
+// Не виходило зробити тому що намагався спочатку робити цикл for of, потім for in, і звідти if. Дивився розв'язок. Прибравши for in все вийшло.
