0493a6c8b914c1480044f9dfca317baf5afbb61b
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-14 12:46:06 +0200

    -made task 1-2

---
+++ Lesson 5/main.js
@@ -0,0 +1,13 @@
+//   #I2XsG6f
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+let calcAreaRectangle = (a, b) => a*b;
+
+console.log(calcAreaRectangle(12, 5));
+
+//   #ETGAxbEn8l
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+
+let calcAreaCircle = (r) => Math.PI*Math.pow(r, 2);
+
+console.log(calcAreaCircle(9));
