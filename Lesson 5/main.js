04b4f6d99cfb2cec32fb8d0d49e996e1751767dd
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-15 16:37:49 +0200

    -made task 7

--- Lesson 5/main.js
+++ Lesson 5/main.js
@@ -49,4 +49,16 @@ let listCreator = (text) =>
 
 listCreator(`list`);
 
+/* #0Kxco1edSN
+- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл) */
 
+let listCreat = (text, listValue) => {
+    document.write(`<ul>`);
+    for (let i = 0; i < listValue; i++) {
+        document.write(`<li>${text}</li>`);
+    }
+    document.write(`</ul>`);
+}
+
+listCreat (`Lorem ipsum dolor sit amet, consectetur.`, 9);
