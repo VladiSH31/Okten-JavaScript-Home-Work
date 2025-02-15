879ccf54636b31b3560b8c516ea83fe7812ad05f
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-15 16:13:14 +0200

    -made task 4-6

--- Lesson 5/main.js
+++ Lesson 5/main.js
@@ -1,21 +1,52 @@
 //   #I2XsG6f
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// noinspection LanguageDetectionInspection
 
-let calcAreaRectangle = (a, b) => a*b;
+let calcAreaRectangle = (a, b) => a * b;
 
 console.log(calcAreaRectangle(12, 5));
 
 //   #ETGAxbEn8l
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 
-let calcAreaCircle = (r) => Math.PI*Math.pow(r, 2);
+let calcAreaCircle = (r) => Math.PI * Math.pow(r, 2);
 
 console.log(calcAreaCircle(9));
 
 // #Mbiz5K4yFe7
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 
-let calcAreaCylinder = (r, h) => 2 * Math.PI*r*h;
-
+let calcAreaCylinder = (r, h) => 2 * Math.PI * r * h;
 
 console.log(calcAreaCylinder(22, 10));
+
+// #SIdMd0hQ
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+let arr = [1, 2, -2, 'hello world', false]
+
+let logArray = (array) => console.log(array);
+
+logArray(arr);
+
+// #59g0IsA
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+let textCreator = (text) => document.write(`<p>${text}</p>`);
+
+textCreator (`Lorem ipsum dolor.`);
+
+/* #hOL6126
+ - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий */
+
+let listCreator = (text) =>
+    document.write(
+            `<ul>
+                <li>${text}</li>
+                <li>${text}</li>
+                <li>${text}</li>
+            </ul>`);
+
+listCreator(`list`);
+
+
