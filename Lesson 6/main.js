f4868eee823ec5b13be917b2e466c46647b0def1
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-18 14:45:04 +0200

    -made task 8

--- Lesson 6/main.js
+++ Lesson 6/main.js
@@ -25,8 +25,8 @@ console.log(str2.toUpperCase());
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
 
 let strUpper = 'HELLO WORLD';
-let strUpper1  = 'LOREM IPSUM';
-let strUpper2  = 'JAVASCRIPT IS COOL';
+let strUpper1 = 'LOREM IPSUM';
+let strUpper2 = 'JAVASCRIPT IS COOL';
 
 console.log(strUpper.toLowerCase());
 console.log(strUpper1.toLowerCase());
@@ -50,13 +50,14 @@ let string = 'Ревуть воли як ясла повні';
 function stringToArray(str) {
     return str.split(' ')
 }
+
 let arr = stringToArray(string);
 console.log(arr);
 
 // #Rbr5kEQ
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
 
-let nums = [10,8,-7,55,987,-1011,0,1050,0]
+let nums = [10, 8, -7, 55, 987, -1011, 0, 1050, 0]
 
 console.log(nums.map(number => number + ''));
 
@@ -67,19 +68,58 @@ console.log(nums.map(number => number + ''));
 // sortNums(nums,'descending') // [21,11,3]
 
 
-
-function sortNums(arrayOfNums,direction) {
+function sortNums(arrayOfNums, direction) {
     if (direction === 'ascending') {
-        arrayOfNums.sort((a, b) => a-b)
+        arrayOfNums.sort((a, b) => a - b)
     }
     if (direction === 'descending') {
-        arrayOfNums.sort((a, b) => b-a)
-        }
+        arrayOfNums.sort((a, b) => b - a)
+    }
     return arrayOfNums;
 }
-let numbers = [11,21,3];
+
+let numbers = [11, 21, 3];
 let sortedNumbers = sortNums(numbers, 'descending');
 console.log(sortedNumbers);
 
 // Підглядував у розв'язок.
 
+// #yo06d74c1C
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+let sort = coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+console.log(sort);
+
+let filtredCourses = coursesAndDurationArray.filter(function (value) {
+    return value.monthDuration > 5;
+})
+console.log(filtredCourses);
+
+let mapedCourses = coursesAndDurationArray.map(function (value, index) {
+    return{
+        id : index+1,
+        title: value.title,
+        monthDuration: value.monthDuration
+    }
+})
+console.log(mapedCourses);
