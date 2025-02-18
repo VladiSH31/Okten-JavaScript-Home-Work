3d7b9f694ea7f414f7b0c975e186771926466844
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-18 12:17:29 +0200

    -made task 2

--- Lesson 6/main.js
+++ Lesson 6/main.js
@@ -11,3 +11,11 @@ console.log(str1.length);
 
 let str2 = 'javascript is cool and very hard';
 console.log(str2.length);
+
+// #8lld9HMxXWB
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+console.log(str.toUpperCase());
+console.log(str1.toUpperCase());
+console.log(str2.toUpperCase());
