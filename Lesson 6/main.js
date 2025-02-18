5e4df24649c04bf3c92c11b2def286f3d50fa227
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-18 13:00:50 +0200

    -made task 6

--- Lesson 6/main.js
+++ Lesson 6/main.js
@@ -52,3 +52,10 @@ function stringToArray(str) {
 }
 let arr = stringToArray(string);
 console.log(arr);
+
+// #Rbr5kEQ
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+
+let nums = [10,8,-7,55,987,-1011,0,1050,0]
+
+console.log(nums.map(number => number + ''));
