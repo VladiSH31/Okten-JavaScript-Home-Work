839cc4dc677393ca326f36ab9dbc99b13ede73f0
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-18 12:15:15 +0200

    -creat rep lesson 6 -made task 1

---
+++ Lesson 6/main.js
@@ -0,0 +1,13 @@
+// #dFeorS3m7u
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+
+let str = 'hello world';
+console.log(str.length);
+
+let str1 = 'lorem ipsum';
+console.log(str1.length);
+
+let str2 = 'javascript is cool and very hard';
+console.log(str2.length);
