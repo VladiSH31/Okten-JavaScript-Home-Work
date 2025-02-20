ee5d2c64c11d3f7390437696ddf2733478a82629
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-20 10:22:17 +0200

    -made task 2-4

--- Lesson 7/main.js
+++ Lesson 7/main.js
@@ -33,8 +33,60 @@ set.add(user8);
 set.add(user9);
 set.add(user10);
 
-let arrayFromSet = Array.from(set);
+let users = Array.from(set);
 
-console.log(arrayFromSet);
+console.log(users);
 
 // Не так як у розв'язку а але результат здається правильний
+
+// #2ikXsE2WiKZ
+// - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+
+let filtredUsers = [];
+for (let user of users) {
+    if (user.id % 2 ===0){
+        filtredUsers.push(user)
+    }
+}
+console.log(filtredUsers);
+
+// #pOeHKct
+// - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
+
+let sorted = users.sort((a, b) => {
+    return b.id - a.id;
+})
+
+console.log(sorted);
+// Зробив спеціально у зворотньому порядку щоб було видно
+
+// #nkMXISv
+// - створити конструктор для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+// створити пустий масив, наповнити його 10 об'єктами Client
+
+class Client {
+
+    constructor(id, name, surname, email, phone, order) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.order = order;
+    }
+
+}
+
+let Clients = [
+    new Client(1, "anna", "white", "anna.white@yahoo.com", "+12-951-6093735", ['milk', 'bread']),
+    new Client(2, "james", "martin", "james.martin@example.com", "+82-815-3464090", ['tomato', 'bread']),
+    new Client(3, "michael", "anderson", "michael.anderson@gmail.com", "+58-378-8078854", ['milk', 'cucumber']),
+    new Client(4, "chris", "harris", "chris.harris@gmail.com", "+7-925-9911410", ['tomato', 'bread']),
+    new Client(5, "james", "jackson", "james.jackson@example.com", "+30-122-7409500", ['milk', 'cucumber']),
+    new Client(6, "john", "martin", "john.martin@yahoo.com", "+48-875-2723929", ['bear', 'meat']),
+    new Client(7, "james", "johnson", "james.johnson@outlook.com", "+2-256-7693455", ['tomato', 'bread']),
+    new Client(8, "david", "brown", "david.brown@example.com", "+61-194-5675450", ['milk', 'meat']),
+    new Client(9, "anna", "martin", "anna.martin@gmail.com", "+88-626-7981931", ['tomato', 'cucumber']),
+    new Client(10, "emily", "smith", "emily.smith@example.com", "+46-534-6364785", ['bear', 'milk']),
+]
+console.log(Clients);
