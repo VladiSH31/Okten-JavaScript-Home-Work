4b4f3b8440d25ab2b5692aaa527f9a34d51faa10
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-21 15:20:58 +0200

    -made task 1

---
+++ Lesson 8/main.js
@@ -0,0 +1,33 @@
+// #AiN5CoUQ
+// Створити функцію, яка робить глибоку копію об'єкту.
+// Додати перевірки на undefined, null, NaN.
+// Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції, які в нього були до цього моменту.
+
+function copyObject(obj) {
+    if (obj === undefined) {
+        console.log('Error');
+    }
+    if (obj === null) {
+        console.log('Error');
+    }
+    // if (isNaN(obj)) {
+    //     console.log('Error');
+    // }
+    else {
+        return {...obj};
+    }
+
+}
+
+let user = {
+    name: 'Oleg',
+    age: 12
+}
+let user1 = {
+    name: 'Mark',
+    age: 10
+}
+let user2 = undefined
+
+let copyUser = copyObject(user2);
+console.log(copyUser);
