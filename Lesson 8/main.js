bfa1d1e1fbf2ceaea78a455bb197d0028376424c
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-21 23:41:12 +0200

    -edit task 1 whith video

--- Lesson 8/main.js
+++ Lesson 8/main.js
@@ -3,34 +3,60 @@
 // Додати перевірки на undefined, null, NaN.
 // Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції, які в нього були до цього моменту.
 
-function copyObject(obj) {
-    if (obj === undefined) {
-        console.log('Error');
-    }
-    if (obj === null) {
-        console.log('Error');
-    }
-    // if (isNaN(obj)) {
-    //     console.log('Error');
-    // }
-    else {
-        return {...obj};
-    }
+// function copyObject(obj) {
+//     if (obj === undefined) {
+//         console.log('Error');
+//     }
+//     if (obj === null) {
+//         console.log('Error');
+//     }
+//     // if (isNaN(obj)) {
+//     //     console.log('Error');
+//     // }
+//     else {
+//         return {...obj};
+//     }
+//
+// }
+//
+// let user = {
+//     name: 'Oleg',
+//     age: 12,
+//     foo() {
+//         console.log(`My name ${name}`);
+//     }
+// }
+// let user1 = {
+//     name: 'Mark',
+//     age: 10
+// }
+// let user2 = undefined
+//
+// let copyUser = copyObject(user);
+// console.log(copyUser);
 
+function cloneObj(obj) {
+    if (obj) {
+        let functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                let functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+        console.log(functions);
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        for (const func of functions) {
+            cloneObj[func.key] = func.functionClone;
+        }
+        console.log(cloneObj);
+        return cloneObj;
+    }
+    throw new Error('Error!!! Enter new Object!!!')
 }
 
-let user = {
-    name: 'Oleg',
-    age: 12,
-    foo() {
-        console.log(`My name ${name}`);
-    }
-}
-let user1 = {
-    name: 'Mark',
-    age: 10
-}
-let user2 = undefined
+let user = {id:12, name: "Oleg", surname:'Fil', greeting() {console.log('Hi')}, filter() {console.log('Lets filter')},}
 
-let copyUser = copyObject(user);
-console.log(copyUser);
+cloneObj(user);
+
+// Дуже тяжке для мене завдання, половину коду я тупо переписував, і не докінця розумію цей код
