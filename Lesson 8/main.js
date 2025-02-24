5185c7c54f73b50dd177e14d0658c91dc2c29330
VladiSH31 <vladislav.sheremet@ukr.net>
2025-02-24 16:00:16 +0200

    -made task 2

--- Lesson 8/main.js
+++ Lesson 8/main.js
@@ -59,4 +59,29 @@ let user = {id:12, name: "Oleg", surname:'Fil', greeting() {console.log('Hi')}, 
 
 cloneObj(user);
 
-// Дуже тяжке для мене завдання, половину коду я тупо переписував, і не докінця розумію цей код
+// Дуже тяжке для мене завдання, половину коду я тупо переписував, і не до кінця розумію цей код
+
+// #iz6emEsP2BA
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
+//     Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+console.log(coursesAndDurationArray.map((course, index) => ({...course, id: index + 1})));
+//Перевіряв з відео
